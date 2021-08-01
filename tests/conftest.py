f4edb3a7efc69ebb5effbc9692e6ac381dddafc0
szypkiwonsz <kacpersawicki321@gmail.com>
2021-08-01 15:25:22 +0200

    Add fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import pytest
+
+from logger.data_handlers import JsonHandler
+from logger.log_entry import LogEntry
+
+
+@pytest.fixture
+def json_handler():
+    temp_json_handler = JsonHandler('test_file.json')
+    return temp_json_handler
+
+
+@pytest.fixture
+def log_entries_json_data():
+    json_log_entries = '''[
+        {
+            "date": "2021-07-31 12:12:19",
+            "level": "CRITICAL",
+            "msg": "xd"
+        }
+    ]'''
+    return json_log_entries
+
+
+@pytest.fixture
+def log_entry():
+    temp_log_entry = LogEntry(datetime(2021, 1, 1), 'ERROR', 'test_message')
+    return temp_log_entry
