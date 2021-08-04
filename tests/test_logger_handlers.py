06cc0919c1fd6ecf5734993f2d01fee1b76d802a
szypkiwonsz <kacpersawicki321@gmail.com>
2021-08-04 21:07:13 +0200

    Add more tests for function
    More tests for function find_by_text_with_msg() of class LoggerReader has been added.

--- tests/test_logger_handlers.py
+++ tests/test_logger_handlers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import mock
 
 import pytest
@@ -6,9 +7,45 @@ import pytest
 @pytest.mark.logger_reader
 class TestLoggerReader:
 
-    def test_find_by_text(self, log_entries_json_data, json_logger_reader):
+    def test_find_by_text_with_msg(self, log_entries_json_data, json_logger_reader):
         with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
             data_with_msg = json_logger_reader.find_by_text('test_msg')
-            data_without_msg = json_logger_reader.find_by_text('non_existing_msg')
         assert data_with_msg == [{"date": "2021-07-31 12:12:19", "level": "CRITICAL", "msg": "test_msg"}]
+
+    def test_find_by_text_without_msg(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_without_msg = json_logger_reader.find_by_text('non_existing_msg')
         assert data_without_msg == []
+
+    def test_data_with_s_date_true(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_with_s_date_true = json_logger_reader.find_by_text('test_msg', start_date=datetime(2020, 7, 31))
+        assert data_with_s_date_true == [{"date": "2021-07-31 12:12:19", "level": "CRITICAL", "msg": "test_msg"}]
+
+    def test_data_with_s_date_false(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_with_s_date_false = json_logger_reader.find_by_text('test_msg', start_date=datetime(2022, 7, 31))
+        assert data_with_s_date_false == []
+
+    def test_data_with_e_date_true(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_with_e_date_true = json_logger_reader.find_by_text('test_msg', end_date=datetime(2022, 7, 31))
+        assert data_with_e_date_true == [{"date": "2021-07-31 12:12:19", "level": "CRITICAL", "msg": "test_msg"}]
+
+    def test_data_with_e_date_false(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_with_e_date_false = json_logger_reader.find_by_text('test_msg', end_date=datetime(2020, 7, 31))
+        assert data_with_e_date_false == []
+
+    def test_data_with_s_date_and_e_date_true(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_with_s_date_and_e_date_true = json_logger_reader.find_by_text(
+                'test_msg', start_date=datetime(2020, 7, 31), end_date=datetime(2022, 7, 31))
+        assert data_with_s_date_and_e_date_true == [
+            {"date": "2021-07-31 12:12:19", "level": "CRITICAL", "msg": "test_msg"}]
+
+    def test_data_with_s_date_and_e_date_false(self, log_entries_json_data, json_logger_reader):
+        with mock.patch("builtins.open", mock.mock_open(read_data=log_entries_json_data), create=True):
+            data_with_s_date_and_e_date_false = json_logger_reader.find_by_text(
+                'test_msg', start_date=datetime(2022, 7, 31), end_date=datetime(2020, 7, 31))
+        assert data_with_s_date_and_e_date_false == []
